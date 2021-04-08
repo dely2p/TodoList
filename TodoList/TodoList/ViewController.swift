277821b4dadb19ab6334e3d237af99a24c20b985
dely2p <lhyal2p@gmail.com>
2021-04-09 00:49:35 +0900

    SettingTableViewCell 및 데이터 추가

--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -13,12 +13,21 @@ import Then
 class ViewController: UIViewController {
 
     var settingTableView: UITableView = UITableView(frame: .zero)
+    var listOfSetting: [String] = ["Support", "About", "Version"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.settingTableView = UITableView(frame: self.settingTableView.frame, style: .insetGrouped)
-        
+        self.setTableView()
         self.addSubViews()
         self.makeConstraints()
+    }
+    
+    private func setTableView() {
+        self.settingTableView = UITableView(frame: self.settingTableView.frame, style: .insetGrouped)
+        self.settingTableView.delegate = self
+        self.settingTableView.dataSource = self
+        self.settingTableView.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.cellIdentifier)
+        self.settingTableView.separatorInset = .init(top: 0, left: 5, bottom: 0, right: 5)
     }
     
     private func addSubViews() {
@@ -32,3 +41,18 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.listOfSetting.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = settingTableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.cellIdentifier) as? SettingTableViewCell else { return UITableViewCell() }
+        cell.bind(listOfSetting[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
